edbe934eb93912e9959fe0a79b7cdd55a437cca2
Name999999 <115118820+Name999999@users.noreply.github.com>
2023-01-18 11:14:15 +1100

    Initial Project Upload
    Added existing project to GitHub

---
+++ Assets/Scripts/LevelManager_Complete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager_Complete : MonoBehaviour
+{
+    //public bool isLightOn1 = true;
+    //public bool isLightOn2 = true;
+
+    public int amountOfLightsOn = 2;
+
+}
---
+++ Assets/Scripts/LightSwitch_Complete.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch_Complete : MonoBehaviour
+{
+    public Light lightToSwitch;
+
+    public GameObject LevelManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //when the player is inside the trigger
+    private void OnTriggerEnter(Collider other)                     
+    {
+                if (lightToSwitch.enabled == false) //off
+                {
+                    lightToSwitch.enabled = true;
+                }
+                //else don't enable the light switch
+                else
+                {
+                    lightToSwitch.enabled = false;
+                }
+            
+        
+    }
+}
