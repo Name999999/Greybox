b6ea983e211e632f1c70b6f235faffc7bbe03047
AsherJans <asherjans6@gmail.com>
2023-01-20 12:17:54 +1100

    Added map to main menu
    Added map to main menu

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
