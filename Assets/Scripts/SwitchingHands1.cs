e74b12d8279fad50530ee4ba30d12faf4e6f0fc5
AsherJans <asherjans6@gmail.com>
2023-04-14 15:56:05 +1000

    Final Boss Hands Work Now!

--- Assets/Scripts/SwitchingHands1.cs
+++ Assets/Scripts/SwitchingHands1.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class SwitchingHands1 : MonoBehaviour
 {
-    public GameObject Hand1;
+    [SerializeField] private GameObject Hand1; // Default
+    [SerializeField] private GameObject Hand2; // Metal Pole
+    [SerializeField] private GameObject Hand3; // Bow
+
+    public enum Hands
+    {
+        Empty,
+        Pole,
+        Bow
+    }
+    public Hands selectedItem = Hands.Empty;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +25,51 @@ public class SwitchingHands1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("1"))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Hand1.SetActive(true);
+            if (selectedItem == Hands.Pole)
+            {
+                selectedItem = Hands.Empty;
+            }
+            else if (selectedItem != Hands.Pole)
+            {
+                selectedItem = Hands.Pole;
+            }
+            ChangeSelectedItem(selectedItem);
         }
-
-        if (Input.GetKey("2"))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Hand1.SetActive(false);
+            if (selectedItem == Hands.Bow)
+            {
+                selectedItem = Hands.Empty;
+            }
+            else if (selectedItem != Hands.Bow)
+            {
+                selectedItem = Hands.Bow;
+            }
+            ChangeSelectedItem(selectedItem);
+        }
+    }
+
+    private void ChangeSelectedItem(Hands itemToSelect)
+    {
+        switch(itemToSelect)
+        {
+            case (Hands.Empty):
+                Hand1.SetActive(true);
+                Hand2.SetActive(false);
+                Hand3.SetActive(false);
+                break;
+            case (Hands.Pole):
+                Hand1.SetActive(false);
+                Hand2.SetActive(true);
+                Hand3.SetActive(false);
+                break;
+            case (Hands.Bow):
+                Hand1.SetActive(false);
+                Hand2.SetActive(false);
+                Hand3.SetActive(true);
+                break;
         }
     }
 }
