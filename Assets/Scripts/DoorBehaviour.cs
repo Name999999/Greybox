68531c41518d61981fec4a9d43e3e69eec9a7756
Name999999 <115118820+Name999999@users.noreply.github.com>
2023-07-10 16:29:51 +1000

    e
    e

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -10,14 +10,42 @@ public class DoorBehaviour : MonoBehaviour
     float _doorSpeed = 10f;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _doorClosedPos = transform.position;
+        _doorOpenPos = new Vector3(transform.position.x,
+            transform.position.y + 3f, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_isDoorOpen)
+        {
+            OpenDoor();
+        }
+        else if (!_isDoorOpen)
+        {
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (transform.position !=_doorOpenPos)
+        {
+          transform.position = Vector3.MoveTowards(transform.position, _doorOpenPos, _doorSpeed * Time.deltaTime);
+        }
+            
+    }
+
+
+    void CloseDoor()
+    {
+        if (transform.position != _doorClosedPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _doorClosedPos, _doorSpeed * Time.deltaTime);
+        }
+
     }
 }
