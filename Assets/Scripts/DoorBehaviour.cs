cc9528200a6dc648b00c1041bf3de91b951da57e
Name999999 <115118820+Name999999@users.noreply.github.com>
2023-07-11 15:05:38 +1000

    Doors added to map

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -14,7 +14,7 @@ public class DoorBehaviour : MonoBehaviour
     {
         _doorClosedPos = transform.position;
         _doorOpenPos = new Vector3(transform.position.x,
-            transform.position.y + 3f, transform.position.z);
+            transform.position.y + 5f, transform.position.z);
     }
 
     // Update is called once per frame
