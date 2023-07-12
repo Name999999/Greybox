cbe9d5d64beb8ecd00d4222af9a77573f920e092
ReduxeStudios <72652130+ReduxeStudios@users.noreply.github.com>
2023-07-12 13:39:58 +1000

    Key Update
    Updated Keys

--- Assets/Scripts/KeyBehaviour.cs
+++ Assets/Scripts/KeyBehaviour.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class KeyBehaviour : MonoBehaviour
 {
+    public GameObject Key1;
+    public GameObject Key2;
+    public GameObject Key3;
+
     [SerializeField] SwitchBehaviour _switchBehaviour;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Key1.SetActive(true);
             _switchBehaviour.DoorLockedStatus();
             Destroy(gameObject);
+
         }
     }
 }
--- Assets/Scripts/StopAnimation.cs
+++ Assets/Scripts/StopAnimation.cs
@@ -7,6 +7,7 @@ public class StopAnimation : MonoBehaviour
     public Animation anim;
     public GameObject MainCamera;
     public GameObject CutsceneCamera;
+    public GameObject DyingCamera;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,18 @@ public class StopAnimation : MonoBehaviour
         MainCamera.SetActive(false);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+           
+                MainCamera.SetActive(false);
+                DyingCamera.SetActive(true);
+                anim.Play("Dying");
+               
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -26,7 +39,9 @@ public class StopAnimation : MonoBehaviour
           CutsceneCamera.SetActive(false);
           MainCamera.SetActive(true);
         }
+
         
+
         // if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Final_Level_Opening_Cutscene"))
         {
            // myAnimation.gameObject.GetComponent<Animator>().enabled = false;
